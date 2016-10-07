20a38e3ecd29a3b5ed5551a9f2d49e87272a9dd4
Behoston <mlegiecki@gmail.com>
2016-10-07 13:24:27 +0200

    My Allegro done (without tests)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # pyllegro
-===
+
 <img src="https://raw.github.com/Behoston/pyllegro/master/misc/logo.png"  height="200" width="200" title="pyllegro logo" />
 
 
@@ -17,3 +17,12 @@
  - Dokumentacja powinna być zawarta w kodzie programu
  - Implementacja powinna zawierać zarówno `webapi` jak i `REST API` i umożliwiać korzystanie z obydwu w dość przezroczysty sposób
  - Wszelkie błędy i niedociągnięcia po stronie API powinny być wyraźnie zaznaczone w miejscu w którym są wykorzystywane
+
+## Jak to działa?
+<img src="https://raw.github.com/Behoston/pyllegro/master/misc/diagram.png" title="diagram" />
+
+Na pomarańczowo oznaczone są elementy obsługiwane po stronie serwisu allegro, kolorem niebieskim elementy pyllegro.<br>
+Allegro udostępnia WebApi za pomocą WSDL, które jest bardzo przestarzałe 
+(użytkownik jest zmuszony podawać hasło do konta, żeby można było zintegrować program z allegro) 
+oraz REST API, które wspiera OAuth. Niestety REST API jest rozwijane bardzo powolnie i na dzień dzisiejszy dostępna jest tylko jedna metoda.
+Po zalogowaniu do REST API (poprzez OAuth) jesteśmy w stanie zalogować się w bezpieczny sposób do WebApi i używać wszystkich dostępnych w nim metod.
--- allegro_user.py
+++ allegro_user.py
@@ -14,7 +14,9 @@ class AllegroUser(object):
     def __repr__(self):
         return 'AllegroUser[' + str(self.user_id) + ']'
 
-    # My Allegro section
+    # ######## My Allegro section ############
+
+
     def get_favourite_categories(self):
         return self.api.client.service.doGetFavouriteCategories(sessionHandle=self.session_id)
 
@@ -38,7 +40,7 @@ class AllegroUser(object):
         :param filter_search:       search condition in title, you can use * - () ""
         :param filter_category_id:  only bid from this category
         :param filter_item_ids:     only for specified items (list)
-        :param page_size:           amount of returned items (min=1, max=1000, default=1000)
+        :param page_size:           amount of returned items (min=1, max=1000, default=100)
         :param page_number:         default 0
         """
         request = {'sessionId': self.session_id}
@@ -73,7 +75,7 @@ class AllegroUser(object):
                                   2 - only shop offers
         :param filter_category_id: only offers from specified category
         :param filter_item_ids:    only offers for specified items (max 100 items)
-        :param page_size:   amount of returned items (min=1, max=1000, default=1000)
+        :param page_size:   amount of returned items (min=1, max=1000, default=100)
         :param page_number: default 0
         """
         request = {'sessionId': self.session_id}
@@ -128,7 +130,7 @@ class AllegroUser(object):
         :param filter_search:       search condition in title, you can use * - () ""
         :param filter_category_id:  only offers from specified category
         :param filter_item_ids:     only offers for specified items (max 100 items)
-        :param page_size:           amount of returned items (min=1, max=1000, default=1000)
+        :param page_size:           amount of returned items (min=1, max=1000, default=100)
         :param page_number:         default 0
         """
         request = {'sessionId': self.session_id}
@@ -180,7 +182,7 @@ class AllegroUser(object):
         :param filter_search:       search condition in title, you can use * - () ""
         :param filter_category_id:  only offers from specified category
         :param filter_item_ids:     only offers for specified items (max 100 items)
-        :param page_size:           amount of returned items (min=1, max=1000, default=1000)
+        :param page_size:           amount of returned items (min=1, max=1000, default=100)
         :param page_number:         default 0
         """
         request = {'sessionId': self.session_id}
@@ -257,7 +259,7 @@ class AllegroUser(object):
         :param filter_search:       search condition in title, you can use * - () ""
         :param filter_category_id:  only offers from specified category
         :param filter_item_ids:     only offers for specified items (max 100 items)
-        :param page_size:           amount of returned items (min=1, max=1000, default=1000)
+        :param page_size:           amount of returned items (min=1, max=1000, default=100)
         :param page_number:         default 0
         """
         request = {'sessionId': self.session_id}
@@ -298,6 +300,254 @@ class AllegroUser(object):
         if page_number is not None:
             request['pageNumber'] = page_number
         return self.api.client.service.doGetMySellItems(**request)
+
+    def get_sold_items(self, sort_by=None, sort_order=None,
+                       filter_offer_type=None, filter_from_end=None,
+                       filter_auto_renew=None, filter_price_from=None, filter_price_to=None, filter_search=None,
+                       filter_category_id=None, filter_item_ids=None,
+                       page_size=None, page_number=None):
+        """
+        :param sort_by: 1  - offer end time (default)       \n
+                        2  - actual price                   \n
+                        3  - offer name                     \n
+                        4  - number of offers               \n
+                        5  - highest offer                  \n
+                        7  - minimum price                  \n
+                        9  - amount offered                 \n
+                        10 - amount sold items
+        :param sort_order:  1 - ascend                      \n
+                            2 - descend (default)
+        :param filter_offer_type: 0 - all (default)         \n
+                                  1 - only Allegro offers   \n
+                                  2 - only shop offers
+        :param filter_from_end: 0  - show all (default)     \n
+                                2  - one hour               \n
+                                3  - three hours            \n
+                                4  - six hours              \n
+                                5  - twelve hours           \n
+                                6  - twenty four hours      \n
+                                7  - two days               \n
+                                8  - three days             \n
+                                9  - four days              \n
+                                10 - five days              \n
+                                11 - six days               \n
+                                12 - seven days
+
+        :param filter_auto_renew: 0 - all offers (default)  \n
+                                  1 - without auto renew    \n
+                                  2 - with auto renew       \n
+                                  3 - with auto renew and full amount
+        :param filter_price_from:   minimum price
+        :param filter_price_to:     maximum price
+        :param filter_search:       search condition in title, you can use * - () ""
+        :param filter_category_id:  only offers from specified category
+        :param filter_item_ids:     only offers for specified items (max 100 items)
+        :param page_size:           amount of returned items (min=1, max=1000, default=100)
+        :param page_number:         default 0
+        """
+        request = {'sessionId': self.session_id}
+        if sort_order is not None or sort_by is not None:
+            request['sortOptions'] = {}
+            if sort_by is not None:
+                request['sortOptions']['sortType'] = sort_by
+            if sort_order is not None:
+                request['sortOptions']['sortOrder'] = sort_order
+        filter_options = {filter_offer_type, filter_from_end, filter_auto_renew, filter_price_from, filter_price_to}
+        if len(filter_options) > 1 or filter_options.pop() is not None:
+            request['filterOptions'] = {}
+            if filter_offer_type is not None:
+                request['filterOptions']['filterFormat'] = filter_offer_type
+            if filter_from_end is not None:
+                request['filterOptions']['filterFromEnd'] = filter_from_end
+            if filter_auto_renew is not None:
+                request['filterOptions']['filterAutoListing'] = filter_auto_renew
+            if filter_price_from is not None or filter_price_to is not None:
+                request['filterOptions']['filterPrice'] = {}
+                if filter_price_from is not None:
+                    request['filterOptions']['filterPrice']['filterPriceFrom'] = filter_price_from
+                if filter_price_to is not None:
+                    request['filterOptions']['filterPrice']['filterPriceTo'] = filter_price_to
+        if filter_search is not None:
+            request['searchValue'] = filter_search
+        if filter_category_id is not None:
+            request['categoryId'] = filter_category_id
+        if filter_item_ids is not None:
+            request['itemIds'] = filter_item_ids
+        if page_size is not None:
+            request['pageSize'] = page_size
+        if page_number is not None:
+            request['pageNumber'] = page_number
+        return self.api.client.service.doGetMySoldItems(**request)
+
+    def get_watched_items(self, sort_by=None, sort_order=None,
+                          filter_search=None, filter_category_id=None, filter_item_ids=None,
+                          page_size=None, page_number=None):
+        """
+        :param sort_by: 1 - offer end time (default)    \n
+                        2 - actual price                \n
+                        3 - offer name                  \n
+                        4 - amount of buy offer         \n
+                        5 - highest buy offer           \n
+                        6 - seller login
+        :param sort_order:  1 - ascend                  \n
+                            2 - descend (default)
+        :param filter_search:       search condition in title, you can use * - () ""
+        :param filter_category_id:  only offers from specified category
+        :param filter_item_ids:     only offers for specified items (max 100 items)
+        :param page_size:           amount of returned items (min=1, max=1000, default=100)
+        :param page_number:         default 0
+        """
+        request = {'sessionId': self.session_id}
+        if sort_order is not None or sort_by is not None:
+            request['sortOptions'] = {}
+            if sort_by is not None:
+                request['sortOptions']['sortType'] = sort_by
+            if sort_order is not None:
+                request['sortOptions']['sortOrder'] = sort_order
+        if filter_search is not None:
+            request['searchValue'] = filter_search
+        if filter_category_id is not None:
+            request['categoryId'] = filter_category_id
+        if filter_item_ids is not None:
+            request['itemIds'] = filter_item_ids
+        if page_size is not None:
+            request['pageSize'] = page_size
+        if page_number is not None:
+            request['pageNumber'] = page_number
+        return self.api.client.service.doGetMyWatchedItems(**request)
+
+    def get_watch_items(self, sort_by=None, sort_order=None,
+                        filter_search=None, filter_category_id=None, filter_item_ids=None,
+                        page_size=None, page_number=None):
+        """
+        :param sort_by: 1 - offer end time (default)    \n
+                        2 - actual price                \n
+                        3 - offer name                  \n
+                        4 - amount of buy offer         \n
+                        5 - highest buy offer           \n
+                        6 - seller login
+        :param sort_order:  1 - ascend  (default)       \n
+                            2 - descend
+        :param filter_search:       search condition in title, you can use * - () ""
+        :param filter_category_id:  only offers from specified category
+        :param filter_item_ids:     only offers for specified items (max 100 items)
+        :param page_size:           amount of returned items (min=1, max=1000, default=100)
+        :param page_number:         default 0
+        """
+        request = {'sessionId': self.session_id}
+        if sort_order is not None or sort_by is not None:
+            request['sortOptions'] = {}
+            if sort_by is not None:
+                request['sortOptions']['sortType'] = sort_by
+            if sort_order is not None:
+                request['sortOptions']['sortOrder'] = sort_order
+        if filter_search is not None:
+            request['searchValue'] = filter_search
+        if filter_category_id is not None:
+            request['categoryId'] = filter_category_id
+        if filter_item_ids is not None:
+            request['itemIds'] = filter_item_ids
+        if page_size is not None:
+            request['pageSize'] = page_size
+        if page_number is not None:
+            request['pageNumber'] = page_number
+        return self.api.client.service.doGetMyWatchItems(**request)
+
+    def get_won_items(self, sort_by=None, sort_order=None,
+                      filter_search=None, filter_category_id=None, filter_item_ids=None,
+                      page_size=None, page_number=None):
+        """
+        :param sort_by: 1  - offer end time (default)    \n
+                        2  - actual price                \n
+                        3  - offer name                  \n
+                        6  - seller login                \n
+                        11 - amount of bought item       \n
+        :param sort_order:  1 - ascend                   \n
+                            2 - descend (default)
+        :param filter_search:       search condition in title, you can use * - () ""
+        :param filter_category_id:  only offers from specified category
+        :param filter_item_ids:     only offers for specified items (max 100 items)
+        :param page_size:           amount of returned items (min=1, max=1000, default=100)
+        :param page_number:         default 0
+        """
+        request = {'sessionId': self.session_id}
+        if sort_order is not None or sort_by is not None:
+            request['sortOptions'] = {}
+            if sort_by is not None:
+                request['sortOptions']['sortType'] = sort_by
+            if sort_order is not None:
+                request['sortOptions']['sortOrder'] = sort_order
+        if filter_search is not None:
+            request['searchValue'] = filter_search
+        if filter_category_id is not None:
+            request['categoryId'] = filter_category_id
+        if filter_item_ids is not None:
+            request['itemIds'] = filter_item_ids
+        if page_size is not None:
+            request['pageSize'] = page_size
+        if page_number is not None:
+            request['pageNumber'] = page_number
+        return self.api.client.service.doGetMyWonItems(**request)
+
+    def my_account_data(self, filter_type=None, filter_item_ids=None, offset=None, limit=None):
+        """
+        :param filter_type: from which tab download data
+                            bid      - already bidding items    \n
+                            won      - bought items             \n
+                            not_won  - not bought items         \n
+                            watch    - already watched items    \n
+                            watch_cl - watched ended items      \n
+                            sell     - already selling items    \n
+                            sold     - sold items               \n
+                            not_sold - not sold items           \n
+                            future   - scheduled items          \n
+        :param filter_item_ids: only offers for specified items (probably max 100 or 25 items)
+        :param offset: from which item start listing (default 0) index form 0,
+                       items always sorted by end time ascend
+        :param limit: how many items display (min=1, max=100, default=25)
+        """
+        request = {'sessionId': self.session_id}
+        if filter_type is not None:
+            request['accountType'] = filter_type
+        if filter_item_ids is not None:
+            request['itemsArray'] = filter_item_ids
+        if offset is not None:
+            request['offset'] = offset
+        if limit is not None:
+            request['limit'] = limit
+        return self.api.client.service.doMyAccount2(**request)
+
+    def get_items_count(self, filter_type=None, filter_item_ids=None):
+        """
+        :param filter_type: from which tab download data
+                            bid      - already bidding items    \n
+                            won      - bought items             \n
+                            not_won  - not bought items         \n
+                            watch    - already watched items    \n
+                            watch_cl - watched ended items      \n
+                            sell     - already selling items    \n
+                            sold     - sold items               \n
+                            not_sold - not sold items           \n
+                            future   - scheduled items          \n
+        :param filter_item_ids: only offers for specified items (probably max 100 or 25 items)
+        """
+        request = {'sessionId': self.session_id}
+        if filter_type is not None:
+            request['accountType'] = filter_type
+        if filter_item_ids is not None:
+            request['itemsArray'] = filter_item_ids
+        return self.api.client.service.doMyAccountItemsCount(**request)
+
+    def remove_watch(self, item_ids):
+        """
+        :param item_ids: item ids list or id to remove watch (max 25 items)
+        """
+        if isinstance(item_ids, int):
+            item_ids = [item_ids]
+        request = {'sessionId': self.session_id, 'itemsIdArray': item_ids}
+        return self.api.client.service.doRemoveFromWatchList(**request)
+
+    # ######## END My Allegro section ############
 
 
 class SafeAllegroUser(AllegroUser):
